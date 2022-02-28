0f0d0a35214fdc5a784e8824b60cbb8e53c47f83
niazmurshedsiam <niazmurshed16@gmail.com>
2022-03-01 02:00:58 +0600

    Type Basics

---
+++ TypeScriptBasic/src/script.ts
@@ -0,0 +1,33 @@
+
+// let PlayerName = "Masrafi";
+// let age = 34;
+// console.log(age);
+// console.log(PlayerName);
+
+
+// let PlayerName;
+// PlayerName = "Masrafi";
+// PlayerName = "Sakib";
+// console.log(PlayerName);
+
+        //  ... Function
+
+// function multiple(a:number, b:number){
+//     return a * b;
+// }
+// console.log(multiple(5,6));
+        
+//  ...  Array
+// let Friends = ["Abir", "Karim"];
+// Friends.push(3);
+// let mixeds = ["Abir", "Karim", 2, true];
+// mixeds.push(3);
+
+        //  .. Object
+
+// let Person={
+//     name:'KArim',
+//     age:34,
+//     student:true
+// }
+// Person.age=55;
