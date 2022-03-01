d37f678e86a87094ba995028524c0990a3e79f9d
niazmurshedsiam <niazmurshed16@gmail.com>
2022-03-02 00:14:54 +0600

    Dynamic Type & Any Type

---
+++ DynamicType & AnyType/output/script.js
@@ -0,0 +1,13 @@
+"use strict";
+let a;
+a = 5;
+a = 'Abir';
+console.log(a);
+let b = [];
+b.push(3);
+b.push('abir');
+let c;
+c: {
+    name: 'Asraf';
+    age: 22;
+}
---
+++ DynamicType & AnyType/src/script.ts
@@ -0,0 +1,18 @@
+let a : any;
+a = 5;
+a = 'Abir';
+console.log(a);
+
+let b : any[] = [];
+b.push(3);
+b.push('abir');
+
+let c:{
+    name:any,
+    age:any
+}
+
+c:{
+    name:'Asraf';
+    age:22
+}
