9390a04bbca0107292f8463ab2b8460b3952796a
niazmurshedsiam <niazmurshed16@gmail.com>
2022-03-02 01:10:05 +0600

    How to use Functions

---
+++ How to use Functions/output/script.js
@@ -0,0 +1,9 @@
+"use strict";
+// const myFun = (a:string,b:string, c?:string)=>{
+//     console.log(`${a}, Hello World ${b}`);
+// }
+// myFun('abir','aftab');
+const myFun = (a, b) => {
+    return a + b;
+};
+myFun(5, 3);
---
+++ How to use Functions/src/script.ts
@@ -0,0 +1,10 @@
+// const myFun = (a:string,b:string, c?:string)=>{
+//     console.log(`${a}, Hello World ${b}`);
+
+// }
+// myFun('abir','aftab');
+
+const myFun = (a:number,b:number):number=>{
+    return a + b;
+}
+myFun(5,3);
