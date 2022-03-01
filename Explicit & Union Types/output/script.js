4dec6432ad9317061207e4f7210cdef7d5fd2acc
niazmurshedsiam <niazmurshed16@gmail.com>
2022-03-01 22:59:30 +0600

    Explict & Union type

---
+++ Explicit & Union Types/output/script.js
@@ -0,0 +1,22 @@
+"use strict";
+//  Explicit & Union Types
+//  Variable
+// let a: number;
+// let b: string;
+// a = 5;
+// b = 'Karim';
+// console.log(a);
+// console.log(b);
+//array
+// let b : number[] = [];                         
+// let a : (number | string)[] = [];
+// a.push(3,'Abir');
+// console.log(a);
+// // console.log(b.push(3));
+//   Object
+let a;
+a = {
+    name: 'abir',
+    age: 23,
+    adult: true
+};
---
+++ Explicit & Union Types/src/script.ts
@@ -0,0 +1,36 @@
+
+                        //  Explicit & Union Types
+
+                        //  Variable
+
+// let a: number;
+// let b: string;
+// a = 5;
+// b = 'Karim';
+// console.log(a);
+// console.log(b);
+
+                         //array
+
+
+// let b : number[] = [];                         
+
+// let a : (number | string)[] = [];
+// a.push(3,'Abir');
+// console.log(a);
+// // console.log(b.push(3));
+
+
+                    //   Object
+
+
+let a : {
+    name:string,
+    age:number,
+    adult:boolean
+}
+a = {
+    name:'abir',
+    age:23,
+    adult:true
+}
