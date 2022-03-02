e268a20c58bf13264fecf09e6a0d249858d29b6d
niazmurshedsiam <niazmurshed16@gmail.com>
2022-03-02 23:42:18 +0600

    typescript classes

---
+++ TypeScript Classes/src/script.ts
@@ -0,0 +1,18 @@
+class Players{
+    name:string;
+    age:number;
+    country:string
+    constructor(name:string,age:number,country:string){
+        this.name = name;
+        this.age = age;
+        this.country = country
+    }
+    play(){
+        console.log(`${this.name} from ${this.country} is playing.`);
+    }
+
+}
+const masrafi = new Players('Mash',34,'Bangladesh');
+
+const Player:Players[]=[];
+Player.push(masrafi);
