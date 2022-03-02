5bc332b1e9facf7af77810bdc50d896f95bb6ce6
niazmurshedsiam <niazmurshed16@gmail.com>
2022-03-02 22:38:56 +0600

    Function Signatures

---
+++ Function Signatures/output/script.js
@@ -0,0 +1,19 @@
+"use strict";
+// let add :(x:number, y:number)=>number;
+// add = (a:number,b:number)=>{
+//     return a+b;
+// }
+// console.log(add(5,6));
+// let calculation:(x:number, y:number, c:string)=>number;
+// calculation=(a:number,b:number,c:string)=>{
+//     if(c==='add'){
+//         return a + b;
+//     }
+//     else{
+//         return a - b;
+//     }
+// }
+// console.log(calculation(5,6,'minus'));
+let UserDetails;
+UserDetails = (id, userInfo) => {
+};
---
+++ Function Signatures/src/script.ts
@@ -0,0 +1,33 @@
+// let add :(x:number, y:number)=>number;
+
+// add = (a:number,b:number)=>{
+//     return a+b;
+// }
+// console.log(add(5,6));
+
+// let calculation:(x:number, y:number, c:string)=>number;
+
+// calculation=(a:number,b:number,c:string)=>{
+//     if(c==='add'){
+//         return a + b;
+//     }
+//     else{
+//         return a - b;
+//     }
+
+// }
+// console.log(calculation(5,6,'minus'));
+
+
+let UserDetails : (id:number|string,userInfo:{
+    name:string,
+    age:number
+})=>void;
+
+UserDetails=(id:number|string,userInfo:{
+    name:string,
+    age:number
+})=>{
+   
+}
+
