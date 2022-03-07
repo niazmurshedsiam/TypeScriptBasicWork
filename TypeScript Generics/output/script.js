2c8296669ee371e7dc400df54653f028754c0c25
niazmurshedsiam <niazmurshed16@gmail.com>
2022-03-08 02:48:34 +0600

    interface for typescript Generics

--- TypeScript Generics/output/script.js
+++ TypeScript Generics/output/script.js
@@ -4,15 +4,11 @@
 // const masrafi = new Players('Mash',34,'Bangladesh');
 // let sakib: IsPlayers;
 // sakib = new Players("Sakib",35,"Bangladesh");
-// const Player:IsPlayers[]=[];
-// Player.push(masrafi);
-const addID = (obj) => {
-    let id = Math.floor(Math.random() * 100);
-    return Object.assign(Object.assign({}, obj), { id });
+const response1 = {
+    status: 200,
+    type: 'good',
+    data: {
+        name: 'test',
+        something: 300
+    }
 };
-let user = addID({
-    name: "Siam",
-    age: 40,
-    country: 'Bangladesh'
-});
-addID(user);
--- TypeScript Generics/src/script.ts
+++ TypeScript Generics/src/script.ts
@@ -6,18 +6,33 @@
 
 // const Player:IsPlayers[]=[];
 // Player.push(masrafi);
-const addID = <T extends {
-    name:string;
-    age:number;
-}>(obj:T)=>{
-    let id = Math.floor(Math.random() * 100);
-    return {...obj,id};
-};
+// const addID = <T extends {
+//     name:string;
+//     age:number;
+// }>(obj:T)=>{
+//     let id = Math.floor(Math.random() * 100);
+//     return {...obj,id};
+// };
 
-let user = addID({
-    name: "Siam",
-    age:40,
-    country: 'Bangladesh'
-});
-addID(user);
+// let user = addID({
+//     name: "Siam",
+//     age:40,
+//     country: 'Bangladesh'
+// });
+// addID(user);
 
+
+interface APIResponse<T>{
+    status: number;
+    type: string;
+    data: T; 
+}
+
+const response1: APIResponse<object> = {
+    status: 200,
+    type: 'good',
+    data:{
+        name: 'test',
+        something: 300
+    }
+}
